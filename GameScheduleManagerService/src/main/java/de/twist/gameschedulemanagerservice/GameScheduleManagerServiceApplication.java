d1e0c7817219d9c05e29da04e300e97074a94591
Oliver <oliver.kuhn@ancud.de>
2023-03-01 17:09:44 +0100

    spring init

---
+++ GameScheduleManagerService/src/main/java/de/twist/gameschedulemanagerservice/GameScheduleManagerServiceApplication.java
@@ -0,0 +1,13 @@
+package de.twist.gameschedulemanagerservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GameScheduleManagerServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GameScheduleManagerServiceApplication.class, args);
+	}
+
+}
