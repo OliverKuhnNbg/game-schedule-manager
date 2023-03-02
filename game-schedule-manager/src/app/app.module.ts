d49bf23087760a6ed0eb7c0ae067c1517ea9b53f
Oliver <oliver.kuhn@ancud.de>
2023-03-02 14:36:04 +0100

    init/angular: add bootstrap

--- game-schedule-manager/src/app/app.module.ts
+++ game-schedule-manager/src/app/app.module.ts
@@ -2,10 +2,12 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { AppMainNavigationComponent } from './components/app-main-navigation/app-main-navigation.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AppMainNavigationComponent
   ],
   imports: [
     BrowserModule
